889501b640dec9512b7203aaad7badfdb3a5275d
Swayanshu004 <swayanshusahoo.pri@gmail.com>
2024-12-24 12:52:32 +0530

    update server URl

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import cors from 'cors'
 
 const app = express()
 const corsOptions = {
-    origin: 'https://promotionlink.vercel.app',
+    origin: process.env.ORIGIN,
     methods: "GET, POST, PATCH, DELETE",
     credential: true
   }
 app.use(cors(corsOptions))
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://promotionlink.vercel.app'); 
+  res.header('Access-Control-Allow-Origin', process.env.ORIGIN); 
   res.header('Access-Control-Allow-Credentials', 'true'); 
   res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
